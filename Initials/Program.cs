b572169a12b099a04915c03f278e6fe649fa504c
Jessie Monson <jessiemonson1980@gmail.com>
2019-03-17 16:14:57 -0500

    final code

--- Initials/Program.cs
+++ Initials/Program.cs
@@ -6,8 +6,27 @@ namespace Initials
     {
         public static void Main(string[] args)
         {
-            Console.ReadLine();
+            Console.Write("Please enter your name.:");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine(Initials(name));
+            
+                Console.ReadLine();
         }
+        public static string Initials(String name1)
+        {
+            String result = "";
+            string[] output = name1.Split(' ');
+            foreach (string s in output)
+            {
+                result += s[0];
+            }
+            
+            return result.ToUpper();
+
+        }
+            
+
 
         // TODO: Create a method that takes in a full name and return the initials in all capital letters.
     }
